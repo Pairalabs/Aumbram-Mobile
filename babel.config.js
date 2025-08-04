8898f3fcf8b1168a17c5d0a73b4f9715181639fd
Subrat Sarangi <subratsarangi71@gmail.com>
2025-08-04 18:39:51 +0530

    - initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      // 'react-native-reanimated/plugin',
+      'react-native-worklets/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets'
+          },
+        },
+      ],
+    ],
+  };
 };
