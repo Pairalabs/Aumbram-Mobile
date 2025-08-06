94abe923b91881b642bc6e0d6b2a40ed34031b88
Subrat Sarangi <subratsarangi@pairalabs.in>
2025-08-06 20:15:22 +0530

    - language(en & hi) and theme setup done.

--- src/navigations/Navigation.tsx
+++ src/navigations/Navigation.tsx
@@ -2,6 +2,11 @@ import React, { FC } from 'react'
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { NavigationContainer } from '@react-navigation/native';
 import { navigationRef } from './NavigationUtils';
+import SplashScreen from '@screens/Public/SplashScreen';
+import OnBoarding from '@screens/Public/OnBoarding';
+import navigationStrings from '@constants/navigationStrings';
+import Login from '@screens/Public/Login';
+import OtpVerify from '@screens/Public/OtpVerify';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,9 +17,12 @@ const Navigation: FC = () => {
                 screenOptions={{
                     headerShown: false
                 }}
-                initialRouteName="Splash"
+                initialRouteName={navigationStrings.PUBLIC.SPLASH}
             >
-                <Stack.Screen name="Splash" component={<></>} />
+                <Stack.Screen name={navigationStrings.PUBLIC.SPLASH} component={SplashScreen} />
+                <Stack.Screen name={navigationStrings.PUBLIC.ONBOARDING} component={OnBoarding} />
+                <Stack.Screen name={navigationStrings.PUBLIC.LOGIN} component={Login} />
+                <Stack.Screen name={navigationStrings.PUBLIC.OTP_VERIFY} component={OtpVerify} />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+		fontFamily: {
+            'Satoshi-Variable': ['Satoshi-Variable'],
+			'Inter-Medium': ['Inter-Medium'],
+			'Inter-Regular': ['Inter-Regular'],
+			'Inter-SemiBold': ['Inter-SemiBold'],
+			'Inter-Thin': ['Inter-Thin'],
+			'Inter-Light': ['Inter-Light'],
+			'Inter-ExtraLight': ['Inter-ExtraLight'],
+			'Inter-ExtraBold': ['Inter-ExtraBold'],
+			'Inter-Bold': ['Inter-Bold'],
+			'Inter-Black': ['Inter-Black'],
+        },
+	},
   },
   plugins: [],
 }
